b8f6d67f701989acc00c98dddd66074e46a65c29
Ronaldo Duarte <138784850+roniduarteor@users.noreply.github.com>
2024-02-25 22:12:45 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,56 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
 
     .then((data)=>{
-        console.log(data)
+        mostrarPersonagens(data)
     })
+
+    .catch((err)=>console.log(err))
 })
+
+function mostrarPersonagens(items){ // items vai receber data, q data é os dados da api
+    const containerPerso = document.getElementById("characters-cards")
+    const limitItem = items.slice(0,9)
+    limitItem.map((item)=>{
+
+        
+
+        const charactersInformations = document.createElement('div')
+        charactersInformations.innerHTML = `
+
+        <section class="principaisInfos">
+
+        <div class="character-img">
+            ${addImage(item.image)}
+        </div>
+        
+        <div class="infos-texto">
+        <h2>${item.name}</h2>
+
+        <h4 class="nickname">Nicknames</h4>
+
+        <p class="nicknamesText">${item.alternate_names}</p>
+
+        <a href="" class="saibaMais"><p class="saibaMaisText">Saiba Mais</p></a>
+        </div>
+        
+    </section>
+        `;
+
+        
+
+        charactersInformations.classList.add('charactersBox')
+        containerPerso.appendChild(charactersInformations)
+    })
+}
+
+function addImage(image){
+    if(image == ""){
+        return `<img src="./img/noImage.png"">`
+    }else{
+        return `<img src=${image}>`
+    }
+}
+
+function charactersLimit(index){
+
+}
